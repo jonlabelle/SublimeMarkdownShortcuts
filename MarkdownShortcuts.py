dacef4522127b71d98f1a3bd1a8bccbf70e94e90
Jon LaBelle <contact@jonlabelle.com>
2018-10-10 18:26:17 -0500

    Revert "Use base class for sublime_plugin.TextCommand (when possible)"
    This reverts commit 90cc902b6c91417963cd3b78c28313888589771b.

--- MarkdownShortcuts.py
+++ MarkdownShortcuts.py
@@ -13,111 +13,40 @@ UNORDER_LIST_PATTERN = re.compile(r'(\s*[-+**]+)(\s+)\S+')
 EMPTY_LIST_PATTERN = re.compile(r'(\s*([-+**]|\d+\.+))\s+$')
 
 
-class BaseTextCommand(sublime_plugin.TextCommand):
+class BoldCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, **kwargs):
-        regions = self.view.sel()
-
-        if any(map(lambda reg: reg.empty(), regions)):
-            regions = [sublime.Region(0, self.view.size())]
-        for region in regions:
-            text = self.view.substr(region)
-            replacement = self.transform(text, **kwargs)
-            self.view.replace(edit, region, replacement)
-
-    def should_show_plugin(self):
-        view = self.view
-        filename = view.file_name()
-        if not filename:
-            return False
-        scopename = view.scope_name(0)
-        if scopename.startswith('text.html.markdown') or filename.endswith('.md') or filename.endswith('.markdown'):
-            return True
-        return False
-
-    def is_visible(self):
-        return self.should_show_plugin()
-
-    def is_enabled(self):
-        return self.should_show_plugin()
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            region_text = self.view.substr(region)
+            if len(region_text) > 3 and region_text[0:2] == '**' and region_text[len(region_text) - 2:len(region_text)] == '**':
+                self.view.replace(edit, region, region_text[2:len(region_text) - 2])
+            else:
+                self.view.replace(edit, region, '**' + region_text + '**')
 
 
-class BoldTextCommand(BaseTextCommand):
+class ItalicCommand(sublime_plugin.TextCommand):
 
-    @staticmethod
-    def transform(text):
-        if len(text) > 3 and text[0:2] == '**' and text[len(text) - 2:len(text)] == '**':
-            return text[2:len(text) - 2]
-        else:
-            return '**' + text + '**'
-
-
-class ItalicTextCommand(BaseTextCommand):
-
-    @staticmethod
-    def transform(text):
-        if len(text) > 1 and text[0:1] == '_' and text[len(text) - 1:len(text)] == '_':
-            return text[1:len(text) - 1]
-        else:
-            return '_' + text + '_'
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            region_text = self.view.substr(region)
+            if len(region_text) > 1 and region_text[0:1] == '_' and region_text[len(region_text) - 1:len(region_text)] == '_':
+                self.view.replace(edit, region, region_text[1:len(region_text) - 1])
+            else:
+                self.view.replace(edit, region, '_' + region_text + '_')
 
 
-class DeletedTextCommand(BaseTextCommand):
+class DeletedCommand(sublime_plugin.TextCommand):
 
-    @staticmethod
-    def transform(text):
-        if len(text) > 3 and text[0:2] == '~~' and text[len(text) - 2:len(text)] == '~~':
-            return text[2:len(text) - 2]
-        else:
-            return '~~' + text + '~~'
-
-
-class Heading2TextCommand(BaseTextCommand):
-
-    @staticmethod
-    def transform(text):
-        if text[0:2] == '# ':
-            return '#' + text
-        if text[0:3] == '## ':
-            return text[3:len(text)]
-        if text[0:4] == '### ':
-            return text[1:len(text)]
-        if text[0:5] == '#### ':
-            return '#' + text[3:len(text)]
-        if text[0:2] != '# ' and text[0:3] != '## ' and text[0:4] != '### ' and text[0:5] != '#### ':
-            return '## ' + text
-
-
-class Heading3TextCommand(BaseTextCommand):
-
-    @staticmethod
-    def transform(text):
-        if text[0:2] == '# ':
-            return '##' + text
-        if text[0:3] == '## ':
-            return '#' + text
-        if text[0:4] == '### ':
-            return text[4:len(text)]
-        if text[0:5] == '#### ':
-            return '#' + text[3:len(text)]
-        if text[0:2] != '# ' and text[0:3] != '## ' and text[0:4] != '### ' and text[0:5] != '#### ':
-            return '### ' + text
-
-
-class Heading4TextCommand(BaseTextCommand):
-
-    @staticmethod
-    def transform(text):
-        if text[0:2] == '# ':
-            return '###' + text
-        if text[0:3] == '## ':
-            return '##' + text
-        if text[0:4] == '### ':
-            return '#' + text
-        if text[0:5] == '#### ':
-            return text[5:len(text)]
-        if text[0:2] != '# ' and text[0:3] != '## ' and text[0:4] != '### ' and text[0:5] != '#### ':
-            return '#### ' + text
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            region_text = self.view.substr(region)
+            if len(region_text) > 3 and region_text[0:2] == '~~' and region_text[len(region_text) - 2:len(region_text)] == '~~':
+                self.view.replace(edit, region, region_text[2:len(region_text) - 2])
+            else:
+                self.view.replace(edit, region, '~~' + region_text + '~~')
 
 
 class SmartListCommand(sublime_plugin.TextCommand):
@@ -126,11 +55,11 @@ class SmartListCommand(sublime_plugin.TextCommand):
         for region in self.view.sel():
             line_region = self.view.line(region)
 
-            # the content before point at the current line
+            # the content before point at the current line.
             before_point_region = sublime.Region(line_region.a, region.a)
             before_point_content = self.view.substr(before_point_region)
 
-            # Disable smart list when folded
+            # Disable smart list when folded.
             folded = False
             for i in self.view.folded_regions():
                 if i.contains(before_point_region):
@@ -164,18 +93,56 @@ class SmartListCommand(sublime_plugin.TextCommand):
         for region in self.view.sel():
             self.view.show(region)
 
-    def should_show_plugin(self):
-        view = self.view
-        filename = view.file_name()
-        if not filename:
-            return False
-        scopename = view.scope_name(0)
-        if scopename.startswith('text.html.markdown') or filename.endswith('.md') or filename.endswith('.markdown'):
-            return True
-        return False
 
-    def is_visible(self):
-        return self.should_show_plugin()
+class Heading2Command(sublime_plugin.TextCommand):
 
-    def is_enabled(self):
-        return self.should_show_plugin()
+    def run(self, edit):
+        region = self.view.line(self.view.sel()[0])
+        current_line_text = self.view.substr(region)
+
+        if current_line_text[0:2] == '# ':
+            self.view.replace(edit, region, '#' + current_line_text)
+        if current_line_text[0:3] == '## ':
+            self.view.replace(edit, region, current_line_text[3:len(current_line_text)])
+        if current_line_text[0:4] == '### ':
+            self.view.replace(edit, region, current_line_text[1:len(current_line_text)])
+        if current_line_text[0:5] == '#### ':
+            self.view.replace(edit, region, '#' + current_line_text[3:len(current_line_text)])
+        if current_line_text[0:2] != '# ' and current_line_text[0:3] != '## ' and current_line_text[0:4] != '### ' and current_line_text[0:5] != '#### ':
+            self.view.replace(edit, region, '## ' + current_line_text)
+
+
+class Heading3Command(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        region = self.view.line(self.view.sel()[0])
+        current_line_text = self.view.substr(region)
+
+        if current_line_text[0:2] == '# ':
+            self.view.replace(edit, region, '##' + current_line_text)
+        if current_line_text[0:3] == '## ':
+            self.view.replace(edit, region, '#' + current_line_text)
+        if current_line_text[0:4] == '### ':
+            self.view.replace(edit, region, current_line_text[4:len(current_line_text)])
+        if current_line_text[0:5] == '#### ':
+            self.view.replace(edit, region, '#' + current_line_text[3:len(current_line_text)])
+        if current_line_text[0:2] != '# ' and current_line_text[0:3] != '## ' and current_line_text[0:4] != '### ' and current_line_text[0:5] != '#### ':
+            self.view.replace(edit, region, '### ' + current_line_text)
+
+
+class Heading4Command(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        region = self.view.line(self.view.sel()[0])
+        current_line_text = self.view.substr(region)
+
+        if current_line_text[0:2] == '# ':
+            self.view.replace(edit, region, '###' + current_line_text)
+        if current_line_text[0:3] == '## ':
+            self.view.replace(edit, region, '##' + current_line_text)
+        if current_line_text[0:4] == '### ':
+            self.view.replace(edit, region, '#' + current_line_text)
+        if current_line_text[0:5] == '#### ':
+            self.view.replace(edit, region, current_line_text[5:len(current_line_text)])
+        if current_line_text[0:2] != '# ' and current_line_text[0:3] != '## ' and current_line_text[0:4] != '### ' and current_line_text[0:5] != '#### ':
+            self.view.replace(edit, region, '#### ' + current_line_text)
